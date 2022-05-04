850c113310085c5e5a0f5414d048570338673ac7
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2022-05-04 11:12:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try{
+            FileReader FR = new FileReader("text.txt");
+            int C = 0;
+            String text = "";
+            while((C = FR.read()) != -1){
+                text += (char)C; //общее кол-во символов
+            }
+
+            String a = text.replaceAll("\\//.+", "");
+            String c = a.replaceAll("(?s)/\\*.*?\\*/", "");
+
+            FileWriter FW = new FileWriter("code.txt");
+            FW.write(c);
+            FW.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
